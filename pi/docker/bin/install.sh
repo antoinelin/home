a4d25c02757dd14e845ac6bd92c493838ce7261c
Antoine Lin <antoine@sidevision.io>
2024-01-21 16:52:14 +0100

    fix docker install to target ubuntu instead of debian

--- pi/docker/bin/install.sh
+++ pi/docker/bin/install.sh
@@ -12,14 +12,14 @@ cd ..
 # Add Docker's official GPG key:
 sudo apt update
 sudo apt upgrade
-sudo apt install ca-certificates curl gnupg
+sudo apt install install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
