a12302ed6c310d6a24f28fa521a16fd5b843f1fb
Antoine Lin <antoine@sidevision.io>
2024-01-21 17:08:48 +0100

    revert

--- pi/docker/bin/install.sh
+++ pi/docker/bin/install.sh
@@ -11,7 +11,8 @@ cd ..
 
 # Add Docker's official GPG key:
 sudo apt update
-sudo apt install install ca-certificates curl gnupg
+sudo apt upgrade
+sudo apt install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -22,9 +23,9 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable docker
 
 # Add user to docker group
